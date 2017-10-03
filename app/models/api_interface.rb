69677fdbf176f455a7941ffc1f7eb312770e0c2b
xxDOOMbox <shawwian@gmail.com>
2017-10-03 14:12:28 -0400

    implement matching tan pants feature

--- app/models/api_interface.rb
+++ app/models/api_interface.rb
@@ -23,8 +23,22 @@ class ApiInterface < ApplicationRecord
     raw_products = JSON.parse(response)['products']
   end
 
-  def self.products(type)
-    response = @@client.search(type, 0, 50)
+  def self.products(type, offset=0, results=50)
+    if type == "pants" || type == "pants jeans"
+      type = "pants jeans"
+      offset += 3100
+    elsif type == "outerwear"
+      offset += 0
+    elsif type == "accessories"
+      offset += 150
+    elsif type == "shoes" || type == "heels"
+      type = "heels"
+      offset += 100
+    else #tops
+      offset += 150
+    end
+
+    response = @@client.search(type, offset, results)
     raw_products = JSON.parse(response)['products']
   end
 
@@ -45,26 +59,94 @@ class ApiInterface < ApplicationRecord
 
   def self.image_urls(style, color)
     if style[:type] == 'accessories'
-
-      {
-        accessories: style[:accessories_url],
-        outerwear: self.parse(self.products('outerwear')).sample['image']['url'],
-        tops: self.parse(self.products('tops')).sample['image']['url'],
-        pants: self.parse(self.products('pants jeans')).sample['image']['url'],
-        shoes: self.parse(self.products('heels')).sample['image']['url']
-      }
+      self.select_outfit_from_accessories(style, color)
 
     elsif style[:type] == 'outerwear'
+      self.select_outfit_from_outerwear(style, color)
+      
+    elsif style[:type] == 'tops'
+      self.select_outfit_from_tops(style, color)
+
+    elsif style[:type] == 'pants'
+      self.select_outfit_from_pants(style, color)
+
+    else #shoes
+      self.select_outfit_from_shoes(style, color)
+    end
+  end
+
+  def self.swap(item, urls)
+    old_url = urls[item]
+    until urls[item] != old_url
+
+      if item == "pants"
+        urls[item] = self.parse(self.products('pants jeans')).sample['image']['url']
+      elsif item == "shoes"
+        urls[item] = self.parse(self.products('heels')).sample['image']['url']
+      else 
+        urls[item] = self.parse(self.products(item.to_s)).sample['image']['url']
+      end 
+
+    end
+
+    urls
+  end
+
+
+######################################################################################################
+##################### SELECTING STYLES BASED ON USER CHOICE###########################################
+######################################################################################################
+
+
+################################  PANTS  ########################################################
+
+  def self.select_outfit_from_pants(style, color)
+    if color == "tan" || color == "brown" || color == "beige"
+
+      {
+        accessories: self.parse(self.products('accessories')).select do |product|
+          product["color"] == "black" ||
+          product["color"] == "blue" ||
+          product["color"] == "brown"
+        end.sample["image"]["url"],
+
+        outerwear: self.parse(self.products('outerwear')).select do |product|
+          product["color"] == "black" ||
+          product["color"] == "blue"
+        end.sample["image"]["url"],
+
+        tops: self.parse(self.products('tops')).select do |product|
+          product["color"] == "black" ||
+          product["color"] == "blue" ||
+          product["color"] == "white" ||
+          product["color"] == "green"
+        end.sample["image"]["url"],
+
+        pants: style[:pants_url],
+
+        shoes: self.parse(self.products('heels')).select do |product|
+          product["color"] == "black"
+        end.sample["image"]["url"]
+      }
+
+    else
 
       {
         accessories: self.parse(self.products('accessories')).sample['image']['url'],
-        outerwear: style[:outerwear_url],
+        outerwear: self.parse(self.products('outerwear')).sample['image']['url'],
         tops: self.parse(self.products('tops')).sample['image']['url'],
-        pants: self.parse(self.products('pants jeans')).sample['image']['url'],
+        pants: style[:pants_url],
         shoes: self.parse(self.products('heels')).sample['image']['url']
       }
 
-    elsif style[:type] == 'tops'
+    end
+  end
+
+################################  TOPS  ########################################################
+
+  def self.select_outfit_from_tops(style, color)
+    if false
+    else 
 
       {
         accessories: self.parse(self.products('accessories')).sample['image']['url'],
@@ -74,10 +156,14 @@ class ApiInterface < ApplicationRecord
         shoes: self.parse(self.products('heels')).sample['image']['url']
       }
 
-    elsif style[:type] == 'pants'
-      self.select_outfit_from_pants(style, color)
+    end
+  end
 
-    else
+##################################  SHOES ########################################################
+
+  def self.select_outfit_from_shoes(style, color)
+    if false
+    else 
 
       {
         accessories: self.parse(self.products('accessories')).sample['image']['url'],
@@ -90,165 +176,38 @@ class ApiInterface < ApplicationRecord
     end
   end
 
-  def self.swap(item, urls)
-    old_url = urls[item]
-    until urls[item] != old_url
-      urls[item] = self.parse(self.products(item.to_s)).sample['image']['url']
-    end
+################################  OUTERWEAR  ########################################################
 
-    urls
-  end
+  def self.select_outfit_from_outerwear(style, color)
+    if false
+    else 
 
-  def self.select_outfit_from_pants(style, color)
-    if color == "tan" || color == "brown" || color == "beige"
-
-      {
-        accessories: self.parse(self.products('accessories')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "brown"
-        end.sample["image"]["url"],
-        outerwear: self.parse(self.products('outerwear')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue"
-        end.sample["image"]["url"],
-        tops: self.parse(self.products('tops')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "white" ||
-          product["color"] == "green"
-        end.sample["image"]["url"],
-        pants: style[:pants_url],
-        shoes: self.parse(self.products('heels')).select do |product|
-          product["color"] == "black"
-        end.sample["image"]["url"]
-      }
-    else
       {
         accessories: self.parse(self.products('accessories')).sample['image']['url'],
-        outerwear: self.parse(self.products('outerwear')).sample['image']['url'],
+        outerwear: style[:outerwear_url],
         tops: self.parse(self.products('tops')).sample['image']['url'],
-        pants: style[:pants_url],
+        pants: self.parse(self.products('pants jeans')).sample['image']['url'],
         shoes: self.parse(self.products('heels')).sample['image']['url']
       }
+
     end
   end
 
-  def self.select_outfit_from_tops(color)
-    if color == "tan" || color == "brown" || color == "beige"
+##################################  ACCESSORIES  ##############################################
+
+  def self.select_outfit_from_accessories(style, color)
+    if false
+    else 
+
       {
-        accessories: self.parse(self.products('accessories')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "brown"
-        end.sample["image"]["url"],
-        outerwear: self.parse(self.products('outerwear')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue"
-        end.sample["image"]["url"],
-        tops: self.parse(self.products('tops')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "white" ||
-          product["color"] == "green"
-        end.sample["image"]["url"],
-        pants: style[:pants_url],
-        shoes: self.parse(self.products('heels')).select do |product|
-          product["color"] == "black"
-        end.sample["image"]["url"]
+        accessories: style[:accessories_url],
+        outerwear: self.parse(self.products('outerwear')).sample['image']['url'],
+        tops: self.parse(self.products('tops')).sample['image']['url'],
+        pants: self.parse(self.products('pants jeans')).sample['image']['url'],
+        shoes: self.parse(self.products('heels')).sample['image']['url']
       }
+
     end
   end
-
-  def self.select_outfit_from_shoes(color)
-    if color == "tan" || color == "brown" || color == "beige"
-      {
-        accessories: self.parse(self.products('accessories')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "brown"
-        end.sample["image"]["url"],
-        outerwear: self.parse(self.products('outerwear')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue"
-        end.sample["image"]["url"],
-        tops: self.parse(self.products('tops')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "white" ||
-          product["color"] == "green"
-        end.sample["image"]["url"],
-        pants: style[:pants_url],
-        shoes: self.parse(self.products('heels')).select do |product|
-          product["color"] == "black"
-        end.sample["image"]["url"]
-      }
-    end
-  end
-
-  def self.select_outfit_from_outerwear(color)
-    if color == "tan" || color == "brown" || color == "beige"
-      {
-        accessories: self.parse(self.products('accessories')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "brown"
-        end.sample["image"]["url"],
-        outerwear: self.parse(self.products('outerwear')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue"
-        end.sample["image"]["url"],
-        tops: self.parse(self.products('tops')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "white" ||
-          product["color"] == "green"
-        end.sample["image"]["url"],
-        pants: style[:pants_url],
-        shoes: self.parse(self.products('heels')).select do |product|
-          product["color"] == "black"
-        end.sample["image"]["url"]
-      }
-    end
-  end
-
-  def self.select_outfit_from_accessories(color)
-    if color == "tan" || color == "brown" || color == "beige"
-      {
-        accessories: self.parse(self.products('accessories')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "brown"
-        end.sample["image"]["url"],
-        outerwear: self.parse(self.products('outerwear')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue"
-        end.sample["image"]["url"],
-        tops: self.parse(self.products('tops')).select do |product|
-          product["color"] == "black" ||
-          product["color"] == "blue" ||
-          product["color"] == "white" ||
-          product["color"] == "green"
-        end.sample["image"]["url"],
-        pants: style[:pants_url],
-        shoes: self.parse(self.products('heels')).select do |product|
-          product["color"] == "black"
-        end.sample["image"]["url"]
-      }
-    end
-  end
-
-
-
-
-
-  def self.fifty_tops
-    response = self.products("tops")
-    self.parse(response)
-  end
-
-
-
-
 
 end
